7a54fb142df5f575abc06e0383beab90a7108f22
ViPandey <ViPandey@201172e1-da46-0410-8252-cd42346d3567>
2008-02-24 03:59:53 +0000

    git-svn-id: http://scalgorithms.googlecode.com/svn@7 201172e1-da46-0410-8252-cd42346d3567

---
+++ trunk/src/test/scala/net/pragyah/scalgorithms/graphs/TestGraphBasicsUndirected.scala
@@ -0,0 +1,70 @@
+package net.pragyah.scalgorithms.graphs
+
+import junit.framework.TestCase
+import junit.framework.Assert._
+
+class TestGraphBasicsUndirected extends TestCase{
+  
+  def testVertices = {
+    val v1 = Vertex[int](1);
+    val v2 =  Vertex[int](2);
+    val v3 =  Vertex[int](3);
+    val v4 =  Vertex[int](4);
+    
+    val vertices:List[Vertex[int]] = v1 :: v2 :: v3 :: v4 ::  Nil 
+    val graph:Graph[int] = Graph(vertices,false)
+
+    graph.addEdge(v1,v2,9)
+    graph.addEdge(v2,v3,12)
+    graph.addEdge(v3,v4,6)
+    graph.addEdge(v4,v2,5)
+    graph.addEdge(v4,v1,14)
+    
+    
+    val v1Edges = v1.getEdges
+    assert(v1Edges.length == 2)
+    
+    val v2Edges = v2.getEdges
+    assert(v2Edges.length == 3)
+
+    val v3Edges = v3.getEdges
+    assert(v3Edges.length == 2)
+
+    val v4dges = v4.getEdges
+    assert(v4dges.length == 3)
+
+  }
+  
+  def testEdges = {
+    val v1 = Vertex[int](1);
+    val v2 =  Vertex[int](2);
+    val v3 =  Vertex[int](3);
+    val v4 =  Vertex[int](4);
+    
+    val vertices:List[Vertex[int]] = v1 :: v2 :: v3 :: v4 ::  Nil 
+    val graph:Graph[int] = Graph(vertices,false)
+
+    graph.addEdge(v1,v2,9)
+    graph.addEdge(v2,v3,12)
+    graph.addEdge(v3,v4,6)
+    graph.addEdge(v4,v2,5)
+    graph.addEdge(v4,v1,14)
+
+    assert(graph.getEdge(v1,v2).get.weight == 9)
+    assert(graph.getEdge(v2,v1).get.weight == 9)
+
+    assert(graph.getEdge(v2,v3).get.weight == 12)
+    assert(graph.getEdge(v3,v2).get.weight == 12)
+
+    assert(graph.getEdge(v3,v4).get.weight == 6)
+    assert(graph.getEdge(v4,v3).get.weight == 6)
+
+    assert(graph.getEdge(v4,v2).get.weight == 5)
+    assert(graph.getEdge(v2,v4).get.weight == 5)
+
+    assert(graph.getEdge(v4,v1).get.weight == 14)
+    assert(graph.getEdge(v1,v4).get.weight == 14)
+
+  }
+  
+}
