831d2150b8029d8e97fb1b7050448b49dfd9b38d
ViPandey <ViPandey@201172e1-da46-0410-8252-cd42346d3567>
2008-02-24 10:19:04 +0000

    git-svn-id: http://scalgorithms.googlecode.com/svn@12 201172e1-da46-0410-8252-cd42346d3567

---
+++ trunk/src/main/scala/net/pragyah/scalgorithms/sorting/MergeSort.scala
@@ -0,0 +1,23 @@
+package net.pragyah.scalgorithms.sorting
+
+class MergeSort[A <% Ordered[A]] extends SortingAlgorithm[A] {
+
+  def sort(l:List[A]): List[A] = {
+    if( l.length <= 1) return l
+    merge(sort(l.dropRight(l.length/2)),sort(l.drop(l.length - l.length/2)))
+  }
+      
+  def merge(l:List[A],r:List[A]) : List[A] = {
+    var left = l
+    var right = r
+    var ret = List[A]()
+
+    while(left != Nil || right != Nil){
+      if(left == Nil){ ret = ret ::: right; right = Nil}
+      else if(right == Nil){ret = ret ::: left; left = Nil}
+      else if(left.head < right.head) {ret = ret :::left.takeWhile(_ < right.head); left = left.dropWhile(_ < right.head)}
+      else {ret = ret :::right.takeWhile(_ < left.head); right = right.dropWhile(_ < left.head)}
+    }
+    ret
+  }
+}
---
+++ trunk/src/test/scala/net/pragyah/scalgorithms/sorting/TestMergeSort.scala
@@ -0,0 +1,19 @@
+package net.pragyah.scalgorithms.sorting
+
+import junit.framework.TestCase
+import junit.framework.Assert._
+
+class TestMergeSort extends TestCase{
+
+  def testIntSorting = {
+    assert(true)
+    val sorter = new MergeSort[int]()
+    val input:List[int] = 10 :: 98 :: 5 :: 123 :: 3 :: 52 :: 52 :: 52 :: 34 :: 1 :: 22 :: 4 :: 6 ::Nil
+    val output = sorter.sort(input)
+    assert(output.length == input.length)
+    input.foreach(i => {assert(input.count(_ == i) == output.count(_ == i))})
+    output.foldLeft[int](output.head)((a,b) => {assert(a <= b);b})
+    assert(true)
+  }
+  
+}
