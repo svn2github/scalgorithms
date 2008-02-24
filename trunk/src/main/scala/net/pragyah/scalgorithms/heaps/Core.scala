0b1227fabf21da3c5ad2c9914c8f11810efa94fb
ViPandey <ViPandey@201172e1-da46-0410-8252-cd42346d3567>
2008-02-24 03:06:53 +0000

    git-svn-id: http://scalgorithms.googlecode.com/svn@5 201172e1-da46-0410-8252-cd42346d3567

--- trunk/src/main/scala/net/pragyah/scalgorithms/heaps/Core.scala
+++ trunk/src/main/scala/net/pragyah/scalgorithms/heaps/Core.scala
@@ -1,10 +1,15 @@
 package net.pragyah.scalgorithms.heaps
 
 trait Heap[A] {
-
   def insert(a:A)
   def extractMin() : Option[A]
   def delete(a:A)
   def minimum : Option[A]
-
+  def decreaseKey(a:A,k:A) 
+  
+  def += (a:A) = insert(a)
+  def -= (a:A) = delete(a)
+  def ++= (l:List[A]) = l.foreach(+= _)
+  def --= (l:List[A]) = l.foreach(-= _)
+  
 }
