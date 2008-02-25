2f6cf74140e1ebf7c279dfd9f227dcffd85b2387
ViPandey <ViPandey@201172e1-da46-0410-8252-cd42346d3567>
2008-02-25 00:54:22 +0000

    git-svn-id: http://scalgorithms.googlecode.com/svn@19 201172e1-da46-0410-8252-cd42346d3567

--- trunk/src/test/scala/net/pragyah/scalgorithms/AllTests.scala
+++ trunk/src/test/scala/net/pragyah/scalgorithms/AllTests.scala
@@ -3,11 +3,16 @@ package net.pragyah.scalgorithms
 import net.pragyah.scalgorithms.heaps._
 import net.pragyah.scalgorithms.graphs._
 import net.pragyah.scalgorithms.graphs.mst._
+import net.pragyah.scalgorithms.graphs.search._
 import net.pragyah.scalgorithms.sorting._
+
 object AllTests {
   
   def suite : junit.framework.Test = {
     val suite = new junit.framework.TestSuite
+    //Sorting
+    suite.addTestSuite(classOf[TestQuickSort])
+    suite.addTestSuite(classOf[TestMergeSort])
     //Heaps
     suite.addTestSuite(classOf[TestBinomialHeap])
     suite.addTestSuite(classOf[TestFibonacciHeap])
@@ -17,9 +22,11 @@ object AllTests {
     //Graph-MST
     suite.addTestSuite(classOf[TestKruskalsAlgorithm])
     suite.addTestSuite(classOf[TestPrimsAlgorithm])
-    //Sorting
-    suite.addTestSuite(classOf[TestQuickSort])
-    suite.addTestSuite(classOf[TestMergeSort])
+    //Graphs-search
+    suite.addTestSuite(classOf[TestBFS])
+    suite.addTestSuite(classOf[TestDFS])
+    suite.addTestSuite(classOf[TestTopolgicalSorting])
+    suite.addTestSuite(classOf[TestStronglyConnectedComponents])
 
     
     return suite
