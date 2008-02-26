f14f22d2293f4af6e5091b57286d1bf64c774e07
ViPandey <ViPandey@201172e1-da46-0410-8252-cd42346d3567>
2008-02-26 01:44:29 +0000

    added apply and += methods
    git-svn-id: http://scalgorithms.googlecode.com/svn@33 201172e1-da46-0410-8252-cd42346d3567

--- trunk/src/main/scala/net/pragyah/scalgorithms/disjointsets/Core.scala
+++ trunk/src/main/scala/net/pragyah/scalgorithms/disjointsets/Core.scala
@@ -2,6 +2,9 @@ package net.pragyah.scalgorithms.disjointsets
 
 trait DisjointSet[A] {
 
+  def += (x:A) = makeSet(x)
+  def apply(x:A):A = findSet(x)
+
   def makeSet(x:A)
   def findSet(x:A):A
   def union(x:A,y:A)
