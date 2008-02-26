e877c7b1a138f852607fa224f44209c5226a4230
ViPandey <ViPandey@201172e1-da46-0410-8252-cd42346d3567>
2008-02-26 01:45:02 +0000

    using actual DisjoingSet instead of a work around
    git-svn-id: http://scalgorithms.googlecode.com/svn@34 201172e1-da46-0410-8252-cd42346d3567

--- trunk/src/main/scala/net/pragyah/scalgorithms/graphs/mst/KruskalsAlgorithm.scala
+++ trunk/src/main/scala/net/pragyah/scalgorithms/graphs/mst/KruskalsAlgorithm.scala
@@ -5,6 +5,7 @@ import scala.collection.mutable.HashMap;
 import scala.collection.mutable.PriorityQueue
 
 import net.pragyah.scalgorithms.trees.Tree;
+import net.pragyah.scalgorithms.disjointsets.DisjointSetForest
 
 
 /*Enqueues all the edges in an ascending order of their weights ( in a Priority Queue). 
@@ -29,15 +30,9 @@ class KruskalsAlgorithm[A] extends MSTAlgo[A] {
     def generateMST(graph:Graph[A]): Tree[Vertex[A]] = {
 
       var A:List[Edge[A]] = List()
-      val map = new HashMap[Vertex[A],int]() //using a map to map the vertex with the id of the 'imaginary' set it is associated with...
-      // didn't get any good disjoint-set datastructure .. will code it later
+      val ds = new DisjointSetForest[Vertex[A]]()
       
-      var count:int = 0
-      
-      graph.vertices.foreach(v => 
-        {map += ((v,count)) 
-        count += 1} 
-      )
+      graph.vertices.foreach(v => ds.makeSet(v)) 
 
       val Q = new PriorityQueue[Edge[A]]() 
       Q ++= graph.edges
@@ -45,22 +40,14 @@ class KruskalsAlgorithm[A] extends MSTAlgo[A] {
       while(!Q.isEmpty){
         val edge = Q.dequeue
         (edge.v1,edge.v2) match{
-            case (v1,v2) if(map.get(v1).get != map.get(v2).get) => {val temp =  map.get(v2).get;
-                                                                    map.transform( (v,c) => { //this is the UNION part .. to b e replaced later with a disjoint set's union method
-                                                                                if(map.get(v).get == temp){
-                                                                                  //updating the 'imaginary' set id for all the co-set-ers of v2(inclusive) to that of v1
-                                                                                  map.get(v1).get
-                                                                                }
-                                                                                else
-                                                                                   map.get(v).get 
-                                                                              })
-                                                                   A = A ::: List(edge)
-                                                               }
-            case _ => 
-        
+            case (v1,v2) if(ds(v1) != ds(v2)) => { 
+                                                      ds.union(v1,v2);
+                                                      A = A ::: List(edge)
+                                                   }
+            case _ =>         
         }
      }
-        
+
       TreeGenerator.generateTrees(graph,A).head
     }  
 
