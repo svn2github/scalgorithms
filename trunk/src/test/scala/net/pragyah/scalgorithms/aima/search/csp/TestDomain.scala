188d82af788899f024bc19f8e39ac6fbd3586a3e
ViPandey <ViPandey@201172e1-da46-0410-8252-cd42346d3567>
2008-03-30 22:41:43 +0000

    changes for immutable domain
    git-svn-id: http://scalgorithms.googlecode.com/svn@58 201172e1-da46-0410-8252-cd42346d3567

--- trunk/src/test/scala/net/pragyah/scalgorithms/aima/search/csp/TestDomain.scala
+++ trunk/src/test/scala/net/pragyah/scalgorithms/aima/search/csp/TestDomain.scala
@@ -8,40 +8,50 @@ class TestDomain extends TestCase{
   def testDomain = {
 
     val variables = "V1"::"V2"::"V3"::Nil
-    val domain = Domain[String,String](variables)
+    var domain = Domain[String,String](variables)
 
     assert(domain("V1").size == 0)
-    domain += ("V1","v1")
+    var d = domain + ("V1","v1")
+    assert(d != domain)
+    domain = d
     assert(domain("V1").size == 1)
     assertTrue(domain("V1").contains("v1"))
     assertTrue(domain.hasEmpty)
     
-    domain -= ("V1","v1")
+    d = domain - ("V1","v1")
+    assert(d != domain)
+    domain = d
     assert(domain("V1").size == 0)
    
-    domain += ("V1","v11")
-    domain += ("V1","v12")
-    domain += ("V1","v13")
+    d = domain + ("V1","v11")
+    assert(d != domain)
+    domain = d
+    d = domain + ("V1","v12")
+    assert(d != domain)
+    domain = d
+    d = domain + ("V1","v13")
+    assert(d != domain)
+    domain = d
     assert(domain("V1").size == 3)
     assert(domain("V1").contains("v11"))
     assert(domain("V1").contains("v12"))
     assert(domain("V1").contains("v13"))
     
-    domain += ("V2","v21")
-    domain += ("V2","v22")
-    domain += ("V2","v23")
+    domain = domain + ("V2","v21")
+    domain = domain + ("V2","v22")
+    domain = domain + ("V2","v23")
     assert(domain("V2").size == 3)
 
-    domain += ("V3","v31")
-    domain += ("V3","v32")
-    domain += ("V3","v33")
+    domain = domain + ("V3","v31")
+    domain = domain + ("V3","v32")
+    domain = domain + ("V3","v33")
     assert(domain("V3").size == 3)
 
     assertFalse(domain.hasEmpty)
 
-    domain -= ("V3","v31")
-    domain -= ("V3","v32")
-    domain -= ("V3","v33")
+    domain = domain - ("V3","v31")
+    domain = domain - ("V3","v32")
+    domain = domain - ("V3","v33")
     assert(domain("V1").size == 3)
     assert(domain("V2").size == 3)
     assert(domain("V3").size == 0)
