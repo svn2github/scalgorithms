8dc71f3b5f771bea10a25bc2f21af1183c896829
ViPandey <ViPandey@201172e1-da46-0410-8252-cd42346d3567>
2008-04-30 07:12:59 +0000

    git-svn-id: http://scalgorithms.googlecode.com/svn@80 201172e1-da46-0410-8252-cd42346d3567

--- trunk/src/main/scala/net/pragyah/scalgorithms/aima/logic/propositional/inference/TruthTable.scala
+++ trunk/src/main/scala/net/pragyah/scalgorithms/aima/logic/propositional/inference/TruthTable.scala
@@ -3,9 +3,9 @@ package net.pragyah.scalgorithms.aima.logic.propositional.inference
 
 import scala.collection.mutable.{HashSet}
 
-class TruthTable extends InferenceAlgorithm{
+class TruthTable extends InferenceAlgorithm[Sentence,Sentence]{
   
- override def entails(kb:KnowledgeBase,alpha:Sentence) : boolean = {
+ override def entails(kb:KnowledgeBase[Sentence],alpha:Sentence) : boolean = {
     
     val symbols = new HashSet[Symbol]();
     symbols ++= kb.asSentence.symbols
@@ -17,7 +17,7 @@ class TruthTable extends InferenceAlgorithm{
     false
   } 
   
-  def checkAll(kb:KnowledgeBase,alpha:Sentence,symbols:List[Symbol],model:Model):boolean = {
+  def checkAll(kb:KnowledgeBase[Sentence],alpha:Sentence,symbols:List[Symbol],model:Model):boolean = {
     if(symbols.size == 0){
       //if KB is true in the model .... find out if alpha is true as well ... if not then KB does not entail alpha
       // though if KB is not true .... the sentence KB => alpha does not make much sense  BUT is deemed to be true
