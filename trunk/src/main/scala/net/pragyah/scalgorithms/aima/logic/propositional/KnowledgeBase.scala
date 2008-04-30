ecd655339f143ad6a4d0e06411a63eb2743b09c4
ViPandey <ViPandey@201172e1-da46-0410-8252-cd42346d3567>
2008-04-30 07:12:34 +0000

    git-svn-id: http://scalgorithms.googlecode.com/svn@79 201172e1-da46-0410-8252-cd42346d3567

--- trunk/src/main/scala/net/pragyah/scalgorithms/aima/logic/propositional/KnowledgeBase.scala
+++ trunk/src/main/scala/net/pragyah/scalgorithms/aima/logic/propositional/KnowledgeBase.scala
@@ -4,14 +4,29 @@ import net.pragyah.scalgorithms.aima.logic.propositional.Operator.{A,V,!,==>,<=>
 
 import scala.collection.mutable.HashSet
 
-class KnowledgeBase{
+object KnowledgeBase{
+ def apply[A <: Sentence]() = new KnowledgeBase[A]()
+}
+
+class KnowledgeBase[A <: Sentence]{
   
-  var sentences = new HashSet[Sentence]()
+  var sentences = new HashSet[A]()
+  private var trueList:List[Symbol] = Nil 
+  private var falseList:List[Symbol] = Nil 
   
-  def tell(sentence:Sentence) =  sentences += sentence
-  def += (sentence:Sentence) =  sentences += sentence
+  def tell(sentence:A) =  sentences += sentence
+  def += (sentence:A) =  sentences += sentence
+  
+  def tellTrue(symbol:Symbol) = trueList = symbol::trueList
+  def tellFalse(symbol:Symbol) = falseList = symbol::falseList
   
   def asSentence:Sentence = {
     new MultiSentence(A,sentences.toList);
   }
+  
+  def trueSymbols = trueList
+  def falseSymbols = falseList
+  
+  override def toString = trueList.foldLeft[String](asSentence.toString + " "+Operator.A+" ")((str,symbol) => str+" "+Operator.A+" "+symbol+" = TRUE ")
+  
 }
